a1eff37cb7f0e97f34e81bc59b898c0af64fa767
VasanthakumarKDP <vkumar@kdpcorp.com>
2024-05-21 14:25:11 +0530

    first commit

---
+++ public/skills.js
@@ -0,0 +1,15 @@
+const skills = [
+  { id: 1, title: "HTML" },
+  { id: 2, title: "CSS" },
+  { id: 3, title: "JavaScript" },
+  { id: 4, title: "React Js " },
+  { id: 5, title: "React Native" },
+
+  { id: 7, title: "Github" },
+  { id: 8, title: "Responsive Design" },
+  { id: 9, title: "Power BI" },
+
+  { id: 12, title: "SQL Reporting" },
+];
+
+export default skills;
---
+++ src/shared/Footer.jsx
@@ -0,0 +1,61 @@
+import linkedinIcon from "../assets/icons-white/linkedin-white.png";
+import githubIcon from "../assets/icons-white/github-white.png";
+import twitterIcon from "../assets/icons-white/twitter-ico.png";
+import blogIcon from "../assets/icons-white/blog-ico.png";
+import youtubeIcon from "../assets/icons-white/yt-ico.png";
+const Footer = () => {
+  return (
+    <div className="bg-black md:h-96 px-7">
+      <div className="max-w-7xl mx-auto py-20 flex flex-col md:flex-row justify-between">
+        {/* logo and description */}
+        <div className="md:w-2/5 my-3">
+          <h4 className="text-white font-bold text-2xl tracking-wide">
+            Murugan & Vasanth
+          </h4>
+          <p className="mt-5 text-sm leading-7 text-[#eee]">
+            Combining Creativity and Technical Expertise to Deliver Outstanding
+            Projects.We are skilled Mobile App Developers, Website Developers,
+            and provide 24/7 Services
+          </p>
+        </div>
+        {/* social icons */}
+        {/* <div className="my-3">
+          <h4 className="text-white font-bold text-2xl tracking-wide ">
+            SOCIAL
+          </h4>
+          <div className="mt-5 flex gap-3">
+            <a href="" className="ml-1">
+              <img src={linkedinIcon} alt="" className="w-7 h-7" />
+            </a>
+            <a href="" className="ml-1">
+              <img src={githubIcon} alt="" className="w-7 h-7" />
+            </a>
+            <a href="" className="ml-1">
+              <img src={twitterIcon} alt="" className="w-7 h-7" />
+            </a>
+            <a href="" className="ml-1">
+              <img src={blogIcon} alt="" className="w-7 h-7" />
+            </a>
+            <a href="" className="ml-1">
+              <img src={youtubeIcon} alt="" className="w-7 h-7" />
+            </a>
+          </div>
+        </div> */}
+      </div>
+
+      <hr className="text-slate-50 opacity-30 px-7" />
+
+      {/* copyright text */}
+      <div>
+        <p className="md:my-10 py-10 md:py-0 text-sm leading-7 text-[#eee] text-center">
+          © Copyright 2024. Made by{" "}
+          <a href="" className="underline">
+            Murugan & Vasanth
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
